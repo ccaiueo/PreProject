8ae140b94246c8c0aa05f3950df08742c1601ae7
ccaiueo <peepuxx@gmail.com>
2019-02-17 00:15:03 +0900

    2019/02/17 ccaiueo いろいろ修正

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -8,14 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.formbean.UserInfo;
 import com.example.demo.service.DBService;
 
 /**
  * トップController.
- * 
+ *
  * @author erika
  *
  */
@@ -24,10 +23,10 @@ import com.example.demo.service.DBService;
 public class AdminController {
 
 	// メモ 返り値をStringにする時はModelAndViewではなくModelを使う。（既存のものは良いが増やせない）
-    
+
     /**
      * セッションのオブジェクト代入格納.
-     * 
+     *
      * @param userInfo
      * @return
      */
@@ -40,24 +39,24 @@ public class AdminController {
 	private DBService service;
 	/** admin共通URL */
 	private static final String DIR = "admin";
-	
-	
 
-	
+
+
+
     /**
      * 初期表示.
      * @return URL
      */
     @RequestMapping(value= DIR + "/dashboard", method=RequestMethod.GET)
     public String dashboard(Model model) {
-    	
+
     	// メニュー用
     	model.addAttribute("dashboard", "active");
         return "index";
     }
-    
+
     /**
-     * ツイートボタン押下.
+     * Tweetボタン押下.
      * @param tweet
      * @param userInfo
      * @return
@@ -66,24 +65,33 @@ public class AdminController {
     public String sec(@ModelAttribute ("tweet2") String tweet2, @ModelAttribute ("tweet") String tweet, @ModelAttribute("userInfo") UserInfo userInfo, Model model) {
 
         model.addAttribute("tweet", tweet);
+        // sessionに設定
         model.addAttribute("userInfo", userInfo);
 
         System.out.println("SEC");
         return "index";
     }
-    
+
+    /**
+     * Register押下
+     *
+     * @param name
+     * @param userInfo
+     * @param model
+     * @return
+     */
     @RequestMapping(value="admin/session", method=RequestMethod.POST)
     public String session(@ModelAttribute ("name") String name, @ModelAttribute("userInfo") UserInfo userInfo, Model model) {
-    	
+
     	System.out.println("SESSION");
-    	
+
     	// 	sessionに格納
     	setUserInfo(userInfo);
-    	
+
     	model.addAttribute("name", name);
         return "blank";
     }
-    
+
 
     /**
      * セッションクローズ.
@@ -92,12 +100,12 @@ public class AdminController {
      */
     @RequestMapping(value="admin/close", method=RequestMethod.POST)
     public String sessionClose(SessionStatus sessionStatus) {
-    	
+
         sessionStatus.setComplete();
         return "index";
-        
+
     }
-    
+
 
     @RequestMapping(value="admin/cards", method=RequestMethod.POST)
     public String cards(Model model) {
@@ -106,11 +114,23 @@ public class AdminController {
     	model.addAttribute("pages", "active");
     	model.addAttribute("customComponents", "show");
         return "cards";
-        
+
     }
-    
+
+    @RequestMapping(value="admin/create/ticket", method=RequestMethod.POST)
+    public String create(Model model) {
+    	// メニュー用
+
+
+    	model.addAttribute("showCategory", "show");
+    	model.addAttribute("activeCreate", "active");
+    	model.addAttribute("activeTiclet", "active");
+        return "createTicket";
+
+    }
+
 //    // Mybatis
 //    System.out.println("mapper(xml): " + service.get());
 //    System.out.println("mapper(@) : " + service.select());
-    
+
 }
