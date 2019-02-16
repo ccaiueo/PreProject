9286ae59df515fcd636b864bf9ff714477bf0b9d
ccaiueo <peepuxx@gmail.com>
2019-02-16 16:00:18 +0900

    2019/02/16 セッション処理追加

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -1,73 +1,100 @@
 package com.example.demo.controller;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.example.demo.controller.EmployeeController.PAGE_TITLE;
-import com.example.demo.entity.BondInfo;
+import com.example.demo.formbean.UserInfo;
 import com.example.demo.service.DBService;
 
+/**
+ * トップController.
+ * 
+ * @author erika
+ *
+ */
 @Controller
-@SessionAttributes({"bondInfoBean"})
+@SessionAttributes("userInfo")
 public class AdminController {
 
+    
+    /**
+     * セッションのオブジェクト代入格納.
+     * 
+     * @param userInfo
+     * @return
+     */
+    @ModelAttribute("userInfo")
+    public UserInfo setUserInfo(UserInfo userInfo){
+        return userInfo;
+    }
+
 	@Autowired
-	DBService service;
-	
-	ModelAndView model = new ModelAndView();
-	
-	
+	private DBService service;
+	/** admin共通URL */
 	private static final String DIR = "admin";
+	ModelAndView model = new ModelAndView();
 
 	
+    /**
+     * 初期表示.
+     * @return URL
+     */
     @RequestMapping(value= DIR + "/show", method=RequestMethod.GET)
-    public ModelAndView show() {
+    public String show() {
 
-    	BondInfo formbean = new BondInfo();
-        formbean.setIssuerCd("070707");
-        model.addObject("bondInfoBean", formbean);
-    	model.addObject("pageTitle", PAGE_TITLE.BOND_REGISTRATION);
-    	model.setViewName("index");
-        return model;
+        return "index";
     }
     
+    /**
+     * ツイートボタン押下.
+     * @param tweet
+     * @param userInfo
+     * @return
+     */
     @RequestMapping(value="admin/sec", method=RequestMethod.POST)
-    public ModelAndView sec(@ModelAttribute ("bondInfoBean") BondInfo formbean) {
+    public String sec(@ModelAttribute ("tweet") String tweet, @ModelAttribute("userInfo") UserInfo userInfo) {
 
-        formbean.setIssuerCd("070707");
-        model.addObject("bondInfoBean", formbean);
-        model.addObject("pageTitle", PAGE_TITLE.BOND_REGISTRATION);
-        model.setViewName("index");
+        model.addObject("tweet", tweet);
+        model.addObject("userInfo", userInfo);
 
-        return model;
+        System.out.println("SEC");
+        return "index";
     }
     
+    @RequestMapping(value="admin/session", method=RequestMethod.POST)
+    public String session(@ModelAttribute ("name") String name, @ModelAttribute("userInfo") UserInfo userInfo) {
+    	
+    	System.out.println("SESSION");
+    	
+    	// 	sessionに格納
+    	setUserInfo(userInfo);
+    	
+    	model.addObject("name", name);
+        return "blank";
+    }
+    
+
+    /**
+     * セッションクローズ.
+     * @param sessionStatus
+     * @return
+     */
     @RequestMapping(value="admin/close", method=RequestMethod.POST)
-    public ModelAndView sessionClose(SessionStatus sessionStatus) {
-
-        model.addObject("pageTitle", PAGE_TITLE.BOND_REGISTRATION);
-        model.setViewName("index");
-        
+    public String sessionClose(SessionStatus sessionStatus) {
+    	
         sessionStatus.setComplete();
-
-        // Mybatis
-        System.out.println("mapper(xml): " + service.get());
-        System.out.println("mapper(@) : " + service.select());
-
-        return model;
-        
-        // 次ああああああアクションを起こした際はセッションが消える
-        // あああああああああ
-        // ああああああああああああああああaaaああああ
+        return "index";
         
     }
+    
+//    // Mybatis
+//    System.out.println("mapper(xml): " + service.get());
+//    System.out.println("mapper(@) : " + service.select());
+    
 }
